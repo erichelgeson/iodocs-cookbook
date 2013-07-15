981258a506497620b3a8070547bdf6a6ca463107
Eric Helgeson <erichelgeson@gmail.com>
2013-07-14 21:29:41 -0500

    corrected recipe name

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license           "Apache 2.0"
 description       "Installs iodocs from Mashery"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.1"
-recipe            "git", "Installs iodocs"
+recipe            "default", "Installs iodocs"
 
 %w{ debian ubuntu }.each do |os|
   supports os
