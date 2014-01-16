3397455debe7390e0a4b3e3679c17c73d9d93c1b
jblakeney <jblakeney@enernoc.com>
2014-01-16 15:31:11 -0500

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "erichelgeson@gmail.com"
 license           "Apache 2.0"
 description       "Installs iodocs from Mashery"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1"
+version           "0.2"
 recipe            "default", "Installs iodocs"
 
 %w{ debian ubuntu }.each do |os|
